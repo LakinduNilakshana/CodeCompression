2a49f05aad1a4c40c38b3335ddd7a4be3ad1c67c
Lakindu_112169 <lakindu.nilakshana@axiatadigitallabs.com>
2022-09-28 21:25:42 +0530

    Read from file and store in array

--- compressor.cpp
+++ compressor.cpp
@@ -1,3 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int count = 0;
+string binCode[128];
+void readFromFile(string fileName);
+
 int main(){
-    std::cout << "Hello World";
+    readFromFile("original.txt");
+    for (int i=0; i<count; i++){
+        cout<<binCode[i]<<endl;
+    }
+
+}
+
+void readFromFile(string fileName){
+    string text;
+    ifstream File(fileName);
+    while(getline(File,text)){
+        binCode[count]=text;
+        count++;
+        cout << text <<endl;
+    }
+    File.close();
 }
